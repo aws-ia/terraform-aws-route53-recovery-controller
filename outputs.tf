6fae9280b6d7eeee9baac117b203c64fb53550d3
drewmullen <drmullen@amazon.com>
2022-01-21 11:24:19 -0500

    fix recovery_cluster module outputs

--- outputs.tf
+++ outputs.tf
@@ -20,30 +20,30 @@ output "readiness_checks" {
 
 output "cluster" {
   description = "Cluster information."
-  value       = can(module.recovery_cluster.cluster) ? module.recovery_cluster.cluster : null
+  value       = can(module.recovery_cluster[0].cluster) ? module.recovery_cluster[0].cluster : null
 }
 
 output "control_panel" {
   description = "Control Panel information."
-  value       = can(module.recovery_cluster.control_panel) ? module.recovery_cluster.control_panel : null
+  value       = can(module.recovery_cluster[0].control_panel) ? module.recovery_cluster[0].control_panel : null
 }
 
 output "routing_controls" {
   description = "Routing controls per cell."
-  value       = can(module.recovery_cluster.routing_controls) ? module.recovery_cluster.routing_controls : null
+  value       = can(module.recovery_cluster[0].routing_controls) ? module.recovery_cluster[0].routing_controls : null
 }
 
 output "safety_rules" {
   description = "Safety rules."
-  value       = can(module.recovery_cluster.safety_rules) ? module.recovery_cluster.safety_rules : null
+  value       = can(module.recovery_cluster[0].safety_rules) ? module.recovery_cluster[0].safety_rules : null
 }
 
 output "health_checks" {
   description = "Health checks."
-  value       = can(module.recovery_cluster.health_checks) ? module.recovery_cluster.health_checks : null
+  value       = can(module.recovery_cluster[0].health_checks) ? module.recovery_cluster[0].health_checks : null
 }
 
 output "r53_aliases" {
   description = "Route53 alias records, if created."
-  value       = can(module.recovery_cluster.r53_aliases) ? module.recovery_cluster.r53_aliases : null
+  value       = can(module.recovery_cluster[0].r53_aliases) ? module.recovery_cluster[0].r53_aliases : null
 }
